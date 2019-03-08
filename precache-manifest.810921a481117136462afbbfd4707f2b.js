0566fa26158f549769cf8827c5f333e1fda0e801
thigott95 <thigott95@gmail.com>
2019-03-08 14:49:21 -0300

    Updates

---
+++ precache-manifest.810921a481117136462afbbfd4707f2b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bead577fe8e04dadb3b0",
+    "url": "/thigott.github.io/provisionamentovr/static/css/main.2304c4e4.chunk.css"
+  },
+  {
+    "revision": "bead577fe8e04dadb3b0",
+    "url": "/thigott.github.io/provisionamentovr/static/js/main.bead577f.chunk.js"
+  },
+  {
+    "revision": "73ef9250f53fe79b1f3b",
+    "url": "/thigott.github.io/provisionamentovr/static/js/runtime~main.73ef9250.js"
+  },
+  {
+    "revision": "6b4d8c12bfb5198d954c",
+    "url": "/thigott.github.io/provisionamentovr/static/css/2.b0b96ae1.chunk.css"
+  },
+  {
+    "revision": "6b4d8c12bfb5198d954c",
+    "url": "/thigott.github.io/provisionamentovr/static/js/2.6b4d8c12.chunk.js"
+  },
+  {
+    "revision": "a5bb7cca967f179c6e7c5d4c60a54259",
+    "url": "/thigott.github.io/provisionamentovr/static/media/logoviareal.a5bb7cca.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/thigott.github.io/provisionamentovr/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/thigott.github.io/provisionamentovr/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/thigott.github.io/provisionamentovr/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/thigott.github.io/provisionamentovr/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/thigott.github.io/provisionamentovr/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "43c9c522c82659b87f40dbfc41886f14",
+    "url": "/thigott.github.io/provisionamentovr/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/thigott.github.io/provisionamentovr/precache-manifest.810921a481117136462afbbfd4707f2b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/thigott.github.io/provisionamentovr/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
